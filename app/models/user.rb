735aeb75b2e863173f4dd4de337728ae9c57c699
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-05-24 17:15:01 +0200

    user model mvp created

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   ROLES = ["user", "techy"]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :first_name, :last_name, :email, :user_name, :role, presence: true
-  validates_format_of :username, :with => /\A [-a-z]+\Z/
+  # validates_format_of :first_name, :last_name, :with => /\A [-a-z]+\Z/
   validates :email, uniqueness: true
   validates :role, inclusion: { in: ROLES }
   validates :user_name, length: { minimum: 5, maximum: 20 }
