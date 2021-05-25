bf1514a84bcd6daa08a9b69482031d3f57eb7271
Maxim Wolotschij <m.wolotschij@web.de>
2021-05-25 18:01:36 +0200

    added a route and action to book a techy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,16 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new
+    @booking.status = "pending"
+    if @booking.save
+      redirect_to offers_index_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
     resources :offers, only: [:index, :show]
-    resources :bookings, only: [:index]
+    resources :bookings, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
