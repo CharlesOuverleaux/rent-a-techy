a34a79e550d4ab7832f9c18a2e3b56d6c3d73d14
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-05-25 12:11:19 +0200

    Created the booking model, dependent to users and offers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :offers
+  has_many :bookings
+
   ROLES = ["user", "techy"]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
