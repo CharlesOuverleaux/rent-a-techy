93af1283235ecfeadc17fd3b99e9295779dfc8cc
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-05-25 13:03:11 +0200

    added offer seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,23 +4,31 @@ TECHY = "techy"
 USER = "user"
 PW = "123456"
 
-def c_user(first, last)
+def c_techy(first, last)
   un = "#{first}#{last}" # username
   un = un.delete(' ')
-  User.create(first_name: first,
-              last_name: last,
-              user_name: un,
-              img_path: "profiles/#{un}.png",
-              email: Faker::Internet.email,
-              role: TECHY,
-              password: PW)
-  puts "created: #{un}"
+  user = User.create(first_name: first,
+                     last_name: last,
+                     user_name: un,
+                     img_path: "profiles/#{un}.png",
+                     email: Faker::Internet.email,
+                     role: TECHY,
+                     password: PW)
+  puts "Techy: #{un}"
+
+  # create offers for techy
+  rand(1..3).times do
+    offer = Offer.create(title: Faker::Computer.platform,
+                         description: Faker::Quote.famous_last_words,
+                         user: user)
+    puts "\t-> added offer"
+  end
 end
 
-c_user('Charles', 'Overlaux')
-c_user('Arif', 'Gömleksiz')
-c_user('Maxim', 'Wolotschij')
-c_user('Hannes', 'Schaletzky')
+c_techy('Charles', 'Overlaux')
+c_techy('Arif', 'Gömleksiz')
+c_techy('Maxim', 'Wolotschij')
+c_techy('Hannes', 'Schaletzky')
 
 # done
 puts "\nSeeding done."
