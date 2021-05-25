4f91830e1d6aa76f81f676c8377e2b9090145a8f
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-05-25 17:07:17 +0200

    finished seed with bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,10 @@ require 'faker'
 TECHY = "techy"
 USER = "user"
 PW = "123456"
+STATUS = ["pending", "confirmed", "declined"]
 
-def c_techy(first, last)
+# method to create customers and techies
+def c_users(first, last, isCustomer = false)
   un = "#{first}#{last}" # username
   un = un.delete(' ')
   user = User.create(first_name: first,
@@ -12,23 +14,43 @@ def c_techy(first, last)
                      user_name: un,
                      img_path: "profiles/#{un}.png",
                      email: Faker::Internet.email,
-                     role: TECHY,
+                     role: isCustomer ? USER : TECHY,
                      password: PW)
-  puts "Techy: #{un}"
+  puts "#{isCustomer ? 'Customer' : 'Techy'}: #{un}"
 
-  # create offers for techy
-  rand(1..3).times do
-    offer = Offer.create(title: Faker::Computer.platform,
-                         description: Faker::Quote.famous_last_words,
-                         user: user)
-    puts "\t-> added offer"
+  # offers (only for techies)
+  unless isCustomer
+    rand(1..3).times do
+      offer = Offer.create(title: Faker::Computer.platform,
+                           description: Faker::Quote.famous_last_words,
+                           user: user)
+      puts "\t-> added offer"
+    end
   end
 end
 
-c_techy('Charles', 'Overlaux')
-c_techy('Arif', 'Gömleksiz')
-c_techy('Maxim', 'Wolotschij')
-c_techy('Hannes', 'Schaletzky')
+# techies
+c_users('Charles', 'Overlaux')
+c_users('Arif', 'Gömleksiz')
+c_users('Maxim', 'Wolotschij')
+c_users('Hannes', 'Schaletzky')
+
+# customers
+puts "\n"
+5.times do
+  names = Faker::FunnyName.name.split(' ')
+  c_users(names.first, names.last, true)
+end
+
+# bookings
+customers = User.where(role: USER)
+20.times do
+  Booking.create(user: customers.sample,
+                 offer: Offer.all.sample,
+                 booking_date:"#{rand(1..28)}/#{rand(3..10)}/20#{rand(14..21)}",
+                 status: STATUS.sample)
+end
+puts "\nCreated 20 bookings"
 
 # done
 puts "\nSeeding done."
