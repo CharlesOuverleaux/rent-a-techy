e9d2d62b238fb43860d7f069720ff0a21f1de8f8
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-05-25 12:12:18 +0200

    added img_path_to_user

--- db/migrate/20210524140411_add_column_to_user.rb
+++ db/migrate/20210524140411_add_column_to_user.rb
@@ -4,5 +4,6 @@ class AddColumnToUser < ActiveRecord::Migration[6.0]
     add_column :users, :last_name, :string
     add_column :users, :user_name, :string
     add_column :users, :role, :string
+    add_column :users, :img_path, :string
   end
 end
