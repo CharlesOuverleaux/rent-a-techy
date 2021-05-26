427d6a20d5465d849d2fcadce1d18fcbb397dd5e
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-05-26 12:56:49 +0200

    added the routes destroy for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :offers, only: [:index, :show] do
     resources :bookings, only: [:create]
   end
-  resources :bookings, only: [:index, :new]
+  resources :bookings, only: [:index, :new, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
