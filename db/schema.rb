5ef9982e11576e32c3d6063ec6b0fcaad8693b8e
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-05-27 14:46:03 +0200

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_091529) do
+ActiveRecord::Schema.define(version: 2021_05_27_124334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_05_27_091529) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "available_now"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
