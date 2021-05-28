7b2c117fde422184dab97716ac205c77dd40a65e
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-05-28 16:11:30 +0200

    notifications

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :bookings, only: [:create]
   end
   resources :bookings, only: [:index, :new, :destroy]
+  resources :notifications, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
