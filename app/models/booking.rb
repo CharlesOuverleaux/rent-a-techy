035cc59c1d782e54c64815ce9fbb36ed7520e7ae
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2021-05-28 12:09:39 +0200

    added fake confirm/decline logic

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,15 @@ class Booking < ApplicationRecord
   STATUS = ["pending", "confirmed", "declined"]
   validates :status, inclusion: { in: STATUS }
   validates :booking_date, presence: true
+
+  after_commit :change_status_fake, on: [:create]
+
+  def change_status_fake
+    Thread.new do
+      sleep(5)
+      booking = Booking.find(id)
+      booking.status = STATUS[rand(1..2)]
+      booking.save
+    end
+  end
 end
